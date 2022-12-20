312f1efc5d161a853c269f608aefda7e218f7149
daniel <daniel@danielrosnes.no>
2022-12-20 15:14:33 +0100

    Bug fixes

--- backend/web-back/interface/models.py
+++ backend/web-back/interface/models.py
@@ -159,7 +159,7 @@ class Event(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False, null=True)
     time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
     context = models.CharField(max_length=50, null=True)
-    tracked = models.BooleanField()
+    tracked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
